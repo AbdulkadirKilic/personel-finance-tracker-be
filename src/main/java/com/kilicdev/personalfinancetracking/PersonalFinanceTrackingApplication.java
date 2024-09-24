9ce06943b26c521f7e28b76d2d021c336095efb0
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-09-24 21:12:58 +0300

    It was edited to java version 21.

--- src/main/java/com/kilicdev/personalfinancetracking/PersonalFinanceTrackingApplication.java
+++ src/main/java/com/kilicdev/personalfinancetracking/PersonalFinanceTrackingApplication.java
@@ -1,6 +1,5 @@
 package com.kilicdev.personalfinancetracking;
 
-import com.kilicdev.personalfinancetracking.dto.UserDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PersonalFinanceTrackingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalFinanceTrackingApplication.class, args);
-		UserDTO dto = new UserDTO();
-		dto.setId(1L);
-		dto.setName("deneme");
-		dto.setEmail("a@gmail.com");
-		System.out.println(dto);
 	}
 }
