e9b17eacb5085f3100e1e6d326549b0a5101721f
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-10-14 14:47:53 +0300

    Spring validator and SaveUserRequest Added.

---
+++ src/main/java/com/kilicdev/personalfinancetracker/controller/request/SaveUserRequest.java
@@ -0,0 +1,14 @@
+package com.kilicdev.personalfinancetracker.controller.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class SaveUserRequest {
+  @NotNull private String userName;
+  @NotNull private String firstName;
+  @NotNull private String lastName;
+  @NotNull @Email private String email;
+  @NotNull private String password;
+}
