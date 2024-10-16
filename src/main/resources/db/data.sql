442e2b4821b6dd56251c55cea30bd311f5731c55
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-10-16 20:02:33 +0300

    data.sql edited for initial inserts.

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,4 +1,4 @@
 --ROLE TABLE INSERTS
-INSERT INTO finance.role (id, role_name, created_date, created_by) VALUES 
-  (nextval('finance.seq_role'), 'ADMIN', CURRENT_TIMESTAMP, 'system'),
-  (nextval('finance.seq_role'), 'USER', CURRENT_TIMESTAMP, 'system');
+INSERT INTO finance.role (id, version, name, description, created_date, created_by) VALUES
+  (nextval('finance.seq_role'), 0, 'ADMIN','Has admin privileges.', CURRENT_TIMESTAMP, 'system'),
+  (nextval('finance.seq_role'), 0, 'USER','Has standard user privileges.', CURRENT_TIMESTAMP, 'system');
