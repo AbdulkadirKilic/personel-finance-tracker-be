ece7d13338ff6a50b8ecfa461b9a0df4f22cf606
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-10-16 23:20:16 +0300

    TransactionServiceImpl was edited.

--- src/main/java/com/kilicdev/personalfinancetracker/service/TransactionServiceImpl.java
+++ src/main/java/com/kilicdev/personalfinancetracker/service/TransactionServiceImpl.java
@@ -1,9 +1,11 @@
 package com.kilicdev.personalfinancetracker.service;
 
 import com.kilicdev.personalfinancetracker.repository.TransactionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TransactionServiceImpl implements ITransactionService {
 	private TransactionRepository transactionRepository;
 }
